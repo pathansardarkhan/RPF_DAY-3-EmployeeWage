c0a2d67f20d8db973041f1d93529bc2ea5e0d952
pathansardarkhan <pathan.sardarkhan6@gmail.com>
2022-05-12 14:39:29 +0530

    sample

---
+++ src/bridgelabparticeprogram/EmployeeWage_FirstProgram.java
@@ -0,0 +1,10 @@
+package bridgelabparticeprogram;
+
+public class EmployeeWage_FirstProgram {
+
+	public static void main(String[] args) {
+		 System.out.println("Welcome to EmployeeWage program");
+
+	}
+
+}
