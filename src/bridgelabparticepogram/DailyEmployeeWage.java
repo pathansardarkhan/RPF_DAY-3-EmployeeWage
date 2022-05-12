90b5d5594849d5c0b3833c1669dc920c2775eeeb
pathansardarkhan <pathan.sardarkhan6@gmail.com>
2022-05-12 15:02:07 +0530

    sample

---
+++ src/bridgelabparticepogram/DailyEmployeeWage.java
@@ -0,0 +1,29 @@
+package bridgelabparticepogram;
+
+import java.util.Random;
+
+public class DailyEmployeeWage {
+	public static void main(String[] args) {
+		System.out.println("Welcome to EmployeeWage program");
+        int is_Fullpresent=1;
+        int is_Parttime=2;
+        int Wageperhour = 20;
+        int Fulldayhour = 8;
+        int Parttimehour = 4;
+        int Month=20;
+        Random random = new Random();
+        int Empcheck = random.nextInt(3);
+        System.out.println("A random int from 0 to 2: " +  Empcheck);
+        if (Empcheck == is_Fullpresent)
+        {
+            System.out.println("Employee is Present");
+            int DailyEmpWage=Wageperhour*Fulldayhour*Month;
+                    System.out.println("Employee Daily Wage is " +  DailyEmpWage);
+}
+        
+        
+          }
+	}
+		
+
+
