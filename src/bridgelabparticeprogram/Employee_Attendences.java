fc5a1360bcd91f4a1c306ab72140f9a32c3a021a
pathansardarkhan <pathan.sardarkhan6@gmail.com>
2022-05-12 14:32:40 +0530

    sample

---
+++ src/bridgelabparticeprogram/Employee_Attendences.java
@@ -0,0 +1,19 @@
+package bridgelabparticeprogram;
+
+public class Employee_Attendences {
+	public static void main(String[] args)
+    {
+    	int IS_FULL_TIME = 1;
+    	
+    	double empCheck = Math.floor(Math.random() * 10 ) % 2;
+    	
+    	if(empCheck == IS_FULL_TIME) {
+    		System.out.println("Employee is Present");
+    	} else {
+    		System.out.println("Employee is Absent");
+    	}
+    	
+        System.out.println("Welcome to EmployeeWage program");
+    }
+
+}
